b63818a86464875667f7c73f3f11d8c2573acdb1
v-hasu <sunhanxiaoisme@gmail.com>
2014-09-14 15:40:42 +0100

    update the code from Rishabh

--- xapian-letor/letor_internal.h
+++ xapian-letor/letor_internal.h
@@ -40,8 +40,9 @@ class Letor::Internal : public Xapian::Internal::intrusive_base {
 
   public:
 
-    map<string, long int> termfreq(const Xapian::Document & doc, const Xapian::Query & query);
+    std::map<Xapian::docid, double>  letor_score(const Xapian::MSet & mset);
 
+/*
     map<string, double> inverse_doc_freq(const Xapian::Database & db, const Xapian::Query & query);
 
     map<string, long int> doc_length(const Xapian::Database & db, const Xapian::Document & doc);
@@ -62,8 +63,10 @@ class Letor::Internal : public Xapian::Internal::intrusive_base {
 
     double calculate_f6(const Xapian::Query & query, map<string, long int> & tf, map<string, long int> & doc_length, map<string, long int> & coll_tf, map<string, long int> & coll_length, char ch);
 
-    std::map<Xapian::docid, double> letor_score(const Xapian::MSet & mset);
+    map<Xapian::docid, double> letor_score(const Xapian::MSet & mset);
 
+    void letor_learn_model(int svm_type, int kernel_type);
+*/
     void letor_learn_model();
 
     void prepare_training_file(const std::string & query_file, const std::string & qrel_file, Xapian::doccount msetsize);
